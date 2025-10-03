2e18fbdcacbd70a406915ade618d0c71886663fe
tallowa <alubsey@gmail.com>
2025-10-02 22:14:22 -0400

    controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  # Add flash types
+  add_flash_types :info, :error, :warning, :success
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :timezone, :currency])
+  end
+
+  def check_subscription
+    unless current_user.active_subscription?
+      redirect_to pricing_path, alert: "Please upgrade your account to continue."
+    end
+  end
+
+  def check_invoice_limit
+    unless current_user.can_create_invoice?
+      redirect_to invoices_path, alert: "You've reached your monthly invoice limit. Please upgrade to continue."
+    end
+  end
+
+  def check_client_limit
+    unless current_user.can_add_client?
+      redirect_to clients_path, alert: "You've reached your client limit. Please upgrade to add more clients."
+    end
+  end
 end
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+end
